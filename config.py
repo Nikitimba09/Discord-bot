37edca13a81697b9d631ac7874ea556e0085ff56
Nikitimba09 <95375141+Nikitimba09@users.noreply.github.com>
2021-12-28 23:45:50 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+settings = {
+    'token':  # Токен Дискорд бота
+    'bot': 'It-bot',
+    'id': #номер канала в discord,
+    'vk_access_token':, # Токены ВК
+    'prefix': 'Ботик',
+    'vk_id_club': #id группы
+}
